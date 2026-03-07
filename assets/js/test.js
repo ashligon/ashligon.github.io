5d58aa6664dd627d7f6bd50b4ffcb8eb742d580c
Ashleigh Ligon <18335654+ashligon@users.noreply.github.com>
2026-03-07 15:32:00 -0500

    change num label colors when grid item is selected

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,6 +1,8 @@
+
 const container = document.querySelector('.grid-container');
 const clearButton = document.querySelector('#clear-grid-btn');
 const eraserButton = document.querySelector('#eraser-btn');
+const saveButton = document.querySelector('#download-btn');
 const allButtons = document.getElementsByTagName('button');
 
 let gridSize = 28;
@@ -20,25 +22,26 @@ function createGrid() {
         for (let col = 0; col < gridSize; col++) {
             const square = document.createElement('div');
             square.classList.add('grid-item');
+            square.setAttribute('data-grid-col', col);
+            square.setAttribute('data-grid-row', row);
             square.style.backgroundColor = gridBgColor;
 
             // add numbering
-            // TODO: fix formatting when numbers get too big
-            // maybe we only want nums when we export to pdf
-            // if (col == 0) {
-            //     const num_label = document.createElement('div');
-            //     num_label.style.backgroundColor = gridBgColor;
-            //     num_label.style.float = 'left';
-            //     num_label.innerHTML += `<p class='num-label'>${(gridSize-row)}</p>`;
-            //     square.appendChild(num_label);
-            // } else if (row == gridSize-1) {
-            //     const num_label = document.createElement('div');
-            //     num_label.style.backgroundColor = gridBgColor;
-            //     num_label.style.float = 'bottom';
-            //     num_label.innerHTML += `<p class='num-label'>${(col+1)}</p>`;
-            //     square.appendChild(num_label);
-            // }
-
+            if (col == 0) {
+                const num_label = document.createElement('div');
+                num_label.classList.add('num-label');
+                num_label.setAttribute('data-grid-col', col);
+                num_label.setAttribute('data-grid-row', row);
+                num_label.innerHTML += `<p>${(gridSize-row)}</p>`;
+                square.appendChild(num_label);
+            } else if (row == gridSize-1) {
+                const num_label = document.createElement('div');
+                num_label.classList.add('num-label');
+                num_label.setAttribute('data-grid-col', col);
+                num_label.setAttribute('data-grid-row', row);
+                num_label.innerHTML += `<p>${(col+1)}</p>`;
+                square.appendChild(num_label);
+            }
             container.appendChild(square);
         }
     }
@@ -53,13 +56,58 @@ function deleteGrid() {
 }
 
 function drawGridClick(e) {
+  gridCol = e.target.getAttribute('data-grid-col');
+  gridRow = e.target.getAttribute('data-grid-row');
+  const label = document.querySelector(`div.num-label[data-grid-col="${gridCol}"][data-grid-row="${gridRow}"] p`);
+
   if (inkEraser) {
     e.target.style.backgroundColor = gridBgColor;
     e.target.removeAttribute('data-inked');
+    if (label != null) {
+      label.style.color = gridInkColor;
+    }
   } else {
     e.target.style.backgroundColor = gridInkColor;
     e.target.setAttribute('data-inked', 'true');
+    if (label != null) {
+      label.style.color = gridBgColor;
+    }
   }
+}
+
+// clear
+function clearGrid() {
+  if (confirm("are you sure you want to clear your work?")) {
+    gridItems = document.querySelectorAll('.grid-item');
+    for (let i = 0; i < gridItems.length; i++) {
+      gridItems[i].style.backgroundColor = gridBgColor;
+      gridItems[i].removeAttribute('data-inked');
+
+      gridCol = gridItems[i].getAttribute('data-grid-col');
+      gridRow = gridItems[i].getAttribute('data-grid-row');
+      const label = document.querySelector(`div.num-label[data-grid-col="${gridCol}"][data-grid-row="${gridRow}"] p`);
+      if (label) {
+        label.style.color = gridInkColor;
+      }
+    }
+    container.style.backgroundColor = gridBgColor;
+  }
+
+  setTimeout(function () {
+    clearButton.classList.remove('btn-on');
+  }, 800);
+}
+
+// save
+function savePattern() {
+  html2canvas(document.querySelector("#capture")).then(canvas => {
+    var base64image = canvas.toDataURL("image/png");
+    window.open(base64image , "_blank");
+  });
+
+  setTimeout(function () {
+    saveButton.classList.remove('btn-on');
+  }, 800);
 }
 
 // slider
@@ -75,22 +123,6 @@ function rangeSliderValue(value) {
   for (let i = 0; i < gridLabels.length; i++) {
     gridLabels[i].textContent = value;
   }
-}
-
-// clear
-function clearGrid() {
-  if (confirm("are you sure you want to clear your work?")) {
-    gridItems = document.querySelectorAll('.grid-item');
-    for (let i = 0; i < gridItems.length; i++) {
-      gridItems[i].style.backgroundColor = gridBgColor;
-      gridItems[i].removeAttribute('data-inked');
-    }
-    container.style.backgroundColor = gridBgColor;
-  }
-
-  setTimeout(function () {
-    clearButton.classList.remove('btn-on');
-  }, 800);
 }
 
 // listeners
@@ -109,6 +141,7 @@ function listen() {
   });
 
   clearButton.addEventListener('click', clearGrid); 
+  saveButton.addEventListener('click', savePattern);
 
   // toggle button styling when clicked
   for (let i = 0; i < allButtons.length; i++) {
